8edddd9060eafab019f77cbebacf68c11ee8e1cb
GiordanoSM <giordano@suffert.com>
2020-11-11 14:50:10 -0300

    Encriptação com SHA-2 256 padding - Bib

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 # https://cryptography.io/en/latest/
 
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes
 
 def main():
 
   key_size = 1024
   
-  N, e, d = RSAGen(key_size)
+  msg = 'testando msg'
+
+  N, e, d, private_key = RSAGen(key_size)
+
+  ciphertext = Enc(private_key, msg)
 
   print("OK")
+
+  #print(ciphertext)
 
   '''for i in [N, e, d]:
     print(i)'''
@@ -30,9 +37,18 @@ def RSAGen(key_size):
   public_numbers = public_key.public_numbers()
 
   if public_e != public_numbers.e:
-    print("Erro na definição do expoente público.") 
+    print("Erro na definição do expoente público.")
 
-  return public_numbers.n, public_numbers.e, private_numbers.d
+  return public_numbers.n, public_numbers.e, private_numbers.d, private_key
+
+def Enc(private_key, msg):
+
+  public_key = private_key.public_key()
+
+  #Usando sha2
+  ciphertext = public_key.encrypt(msg.encode('utf-8'), padding.OAEP(padding.MGF1(hashes.SHA256()), hashes.SHA256(), None))
+
+  return ciphertext
 
 if __name__ == "__main__":
   main()
